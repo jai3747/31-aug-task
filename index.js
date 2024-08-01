627a6a5e2546792b0cdc62e0563c2cfa66f484d1
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-08-01 08:42:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = 3000;
 
+// Parse cookies
+app.use(cookieParser());
+
+// CSRF protection
+app.use(csrf({ cookie: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // Pass the CSRF token to your view
+  res.send('Hello World! <form action="/submit" method="POST"><input type="hidden" name="_csrf" value="' + req.csrfToken() + '"><input type="submit" value="Submit"></form>');
+});
+
+app.post('/submit', (req, res) => {
+  res.send('Form submitted successfully!');
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+  res.status(403);
+  res.send('Form tampered with');
 });
 
 app.listen(port, () => {
